eca471f18d3cf656d7004a8d55f9b6c04abd0418
ajkarots <jonathan130397@outlook.es>
2025-05-26 22:54:23 -0500

    Se configura la base y se crea el servidor en railway

--- Backend/DB/controllers/Ingreso_Persona.js
+++ Backend/DB/controllers/Ingreso_Persona.js
@@ -1,13 +1,14 @@
 
 const nuevaPersona = async (req, res) => {
     const { idPersona, nombre, apellido, telefono } = req.body;
-$query = `INSERT INTO PERSONAS (ID_PER, NOM_PER,APE_PER, TEL_PER)
-VALUES ('$idPersona', '$nombre', '$apellido', '$telefono')`;
+const query = `INSERT INTO PERSONAS (ID_PER, NOM_PER, APE_PER, TEL_PER)
+VALUES ('${idPersona}', '${nombre}', '${apellido}', '${telefono}')`;
 
-if($conexion->query($query) === TRUE){
-    "Persona registrada correctamente";
-} else {
-    
+try {
+    const result = await conexion.query(query);
+    res.send("Persona registrada correctamente");
+} catch (error) {
+    res.status(500).send("Error al registrar persona");
 }
 
 }
