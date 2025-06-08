f0133664fca46a3d462dfeecfed00c0040e1be16
ajkarots <jonathan130397@outlook.es>
2025-06-08 18:29:43 -0500

    Se añaden los llamados necesarios para subconsultas implicitas y llamados

--- Backend/Controllers/autoridadesController.js
+++ Backend/Controllers/autoridadesController.js
@@ -1,8 +1,20 @@
-const { AUTORIDADES } = require('../models');
+const { sequelize } = require('../models');           // conexión activa
+const initModels = require('../models/init-models');  // función que crea modelos + relaciones
+
+const models = initModels(sequelize);                 // inicializa todo
+const { AUTORIDADES, PERSONAS } = models;             // extraes los modelos que necesitas
+
+
 
 exports.getAll = async (req, res) => {
   try {
-    const data = await AUTORIDADES.findAll();
+    const data = await AUTORIDADES.findAll({
+      include: {
+        model: PERSONAS,
+        as: 'CED_PER_PERSONA', // alias exacto del belongsTo
+        attributes: ['NOM_PER', 'APE_PER']
+      }
+    });
     res.json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
