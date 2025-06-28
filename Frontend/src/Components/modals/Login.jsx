d545b0e3e3adc8982f0963cb0baa0f401f7e6ce2
ajkarots <jonathan130397@outlook.es>
2025-06-28 16:12:27 -0500

    Se corrige problema de conflicto luego de usart stash pop

--- Frontend/src/Components/modals/Login.jsx
+++ Frontend/src/Components/modals/Login.jsx
@@ -4,8 +4,11 @@ import logo from "../../assets/logo.png";
 import { useAuth } from "../../auth/AuthContext";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2"; // Asegúrate de que la URL del backend esté configurada correctamente
+import { FaUserAlt } from "react-icons/fa";
+import Recuperacion from "./Recuperacion";
 
 const Login = ({ isOpen, closeModal }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { login } = useAuth();
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
@@ -27,7 +30,7 @@ const Login = ({ isOpen, closeModal }) => {
 
   const handleLogin = async () => {
     try {
-      const loggedUser= await login(email, password);
+      const loggedUser = await login(email, password);
       const Toast = Swal.mixin({
         toast: true,
         position: "top-end",
@@ -47,10 +50,10 @@ const Login = ({ isOpen, closeModal }) => {
         icon: "success",
         draggable: true,
       }).then(() => {
-        if (loggedUser?.role == "Admin"||loggedUser?.role == "Docente") {
+        if (loggedUser?.role == "Admin" || loggedUser?.role == "Docente") {
           navigate("/Administrador");
           closeModal();
-        }else if(loggedUser?.role=="Estudiante"){
+        } else if (loggedUser?.role == "Estudiante") {
           navigate("/");
           closeModal();
         }
@@ -106,9 +109,13 @@ const Login = ({ isOpen, closeModal }) => {
             </div>
 
             <div className="login-forgot-password">
-              <a href="#" className="login-forgot-link">
-                ¿Olvidaste tu contraseña?
-              </a>
+              <>
+                <button className="login-register-text" onClick={() => setIsModalOpen(true)}>
+                  Recuperar Contraseña
+                </button>
+                <Recuperacion isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}
+                />
+              </>
             </div>
 
             <button className="login-button" onClick={handleLogin}>
@@ -117,6 +124,7 @@ const Login = ({ isOpen, closeModal }) => {
           </div>
 
           <div className="login-register-container">
+
             <p className="login-register-text">
               ¿No tienes cuenta?
               <a href="/Registro" className="login-register-link">
@@ -126,7 +134,8 @@ const Login = ({ isOpen, closeModal }) => {
               <br />
               <a href="/Restudiante" className="login-register-link">
                 {""}
-                Regístrate como estudiante
+                <br></br>
+                Regístrate
               </a>
             </p>
           </div>
